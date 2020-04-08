2be823dbb07e71e01ef6249295319d6bb11123b8
aroge23 <42726350+aroge23@users.noreply.github.com>
2020-04-08 15:59:57 -0700

    4/8/20

--- test.py
+++ test.py
@@ -80,3 +80,41 @@ print(yearlist)
 print(len(tenyield))
 print(len(twoyield))
 print(yearlist[42])
+
+
+
+# with open(tick.upper() + '.csv', 'w', newline='') as file:
+#     writer = csv.writer(file)
+#     writer.writerow([" ", datetime.datetime.today().strftime("%e-%b-%Y")])
+#     for i in range(len(titles)):
+#         writer.writerow([titles[i], values[i]])
+#
+# csv_input = pd.read_csv(tick.upper() + '.csv')
+# # for i in range(len(values)):
+# #     csv_input['today'] = values[i]
+# # csv_input.to_csv(tick.upper() + '.csv', index=False)
+#
+# # # read in the data from file
+# # csv_input[datetime.datetime.today().strftime("%e-%b-%y") + 'test'] = values[0]
+# # csv_input.to_csv(tick.upper() + '.csv', index=False)
+#
+# default_text = 'Some Text'
+# # Open the input_file in read mode and output_file in write mode
+# with open(tick.upper() + '.csv', 'r') as read_obj, \
+#         open('output_1.csv', 'w', newline='') as write_obj:
+#     # Create a csv.reader object from the input file object
+#     csv_reader = csv.reader(read_obj)
+#     # Create a csv.writer object from the output file object
+#     csv_writer = csv.writer(write_obj)
+#     # Read each row of the input csv file as list
+#     for row in csv_reader:
+#         # Append the default text in the row / list
+#         row.append(default_text)
+#         # Add the updated row / list to the output file
+#         csv_writer.writerow(row)
+#
+# data = [line for line in csv.reader('output_1.csv')]
+# # for i in range(len(values)):
+# #     data[i + 1][len(data[0])] = values[i]
+# # write the file back
+# csv.writer(open(tick.upper() + '.csv', 'w')).writerows(data)
--- yahoo.py
+++ yahoo.py
@@ -1,11 +1,16 @@
 #PULLS DATA FROM YAHOO FINANCE AND WRITES TO A CSV FILE
+#CREATES A NEW LINE EVERY TIME YOU RUN IT WITH DATE STAMPS
 import requests
 import lxml.html
 import csv
 import pandas as pd
 import datetime
+import openpyxl as xl
+import os
 
-tick = "wday"
+tick = "aapl"
+csv_fi = tick.upper() + '.csv'
+xlsx_fi = tick.upper() + '.xlsx'
 
 url = "https://in.finance.yahoo.com/quote/" + tick.upper()
 
@@ -36,45 +41,34 @@ td = right_summary.xpath('.//td[@class="C($primaryColor) W(51%)"]')
 for element in td:
     titles.append(element.xpath('.//span/text()')[0])
 
+#CREATES A NEW CSV FILE OF THE GIVEN STOCK IF IT DOESN'T EXIST
+if os.path.exists(csv_fi) == False:
+    with open(csv_fi, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Date"] + titles)
+        writer.writerow([datetime.datetime.today().strftime('%e-%b-%y')] + values)
+else:
+    #CONVERTS CSV INTO EXCEL FOR EASY ACCESS
+    read_file = pd.read_csv(csv_fi)
+    read_file.to_excel(xlsx_fi, index= None, header= True)
 
-with open(tick.upper() + '.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow([" ", datetime.datetime.today().strftime("%e-%b-%Y")])
-    for i in range(len(titles)):
-        writer.writerow([titles[i], values[i]])
+    #CONFIGURES THE EXCEL WORKBOOK
+    wb = xl.load_workbook(xlsx_fi)
+    ws = wb.active
+    ws.title = tick.upper()
 
-csv_input = pd.read_csv(tick.upper() + '.csv')
-# for i in range(len(values)):
-#     csv_input['today'] = values[i]
-# csv_input.to_csv(tick.upper() + '.csv', index=False)
+    #ADDS AN ADDITIONAL ROW OF NEW VALUES EVERY TIME YOU RUN
+    col = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q']
+    ws[col[0] + str(ws.max_row + 1)] = datetime.datetime.today().strftime('%e-%b-%y')
+    for i in range(0, len(values)):
+        ws[col[i + 1] + str(ws.max_row)] = values[i]
 
-# # read in the data from file
-# csv_input[datetime.datetime.today().strftime("%e-%b-%y") + 'test'] = values[0]
-# csv_input.to_csv(tick.upper() + '.csv', index=False)
+    #COMMIT AND SAVE THE EXCEL WORKBOOK
+    wb.save(xlsx_fi)
 
-default_text = 'Some Text'
-# Open the input_file in read mode and output_file in write mode
-with open(tick.upper() + '.csv', 'r') as read_obj, \
-        open('output_1.csv', 'w', newline='') as write_obj:
-    # Create a csv.reader object from the input file object
-    csv_reader = csv.reader(read_obj)
-    # Create a csv.writer object from the output file object
-    csv_writer = csv.writer(write_obj)
-    # Read each row of the input csv file as list
-    for row in csv_reader:
-        # Append the default text in the row / list
-        row.append(default_text)
-        # Add the updated row / list to the output file
-        csv_writer.writerow(row)
+    #CONVERTS BACK INTO CSV FILE
+    read_file = pd.read_excel(xlsx_fi)
+    read_file.to_csv(csv_fi, index= None, header= True)
 
-data = [line for line in csv.reader('output_1.csv')]
-# for i in range(len(values)):
-#     data[i + 1][len(data[0])] = values[i]
-# write the file back
-csv.writer(open(tick.upper() + '.csv', 'w')).writerows(data)
-
-print(titles)
-print(values)
-print(data)
-
-#print(pd.DataFrame({"Names": names, "Prices": prices, "Change": changes, "% Change": percentChanges, "Volume" : totalVolumes}))
+    #DELETE THE TEMPORARY EXCEL FILE
+    os.remove(xlsx_fi)
